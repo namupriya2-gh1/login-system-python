a09f8bd09bc47cad1cfed380f231559e720a7554
Namu Suresh <Namusuresh@Namus-MacBook-Air.local>
2026-04-09 08:41:02 -0700

    Added Reusable login function with multiple users

--- day2_functions.py
+++ day2_functions.py
@@ -1,3 +1,5 @@
+#Login function
+
 def login(User_name, Password):
     if User_name == "Namu" and Password == "1234":
         return "Success!"
@@ -10,3 +12,39 @@ if result == "Success!":
     print("Test Passed")
 else:
     print("Test Failed")
+
+
+#User authentication function
+
+def check_user_role(role):
+    if role == "admin":
+        print("Full access granted.")
+    else:
+        print("Limited access granted.")
+
+
+check_user_role("admin")
+check_user_role("guest")
+
+#Reusable login function with multiple users
+
+def user_login(username, password):
+    if username == "admin" and password == "1234":
+        return "success"
+    else:
+        return "fail"
+
+
+users = [
+    ("admin", "1234"),
+    ("guest", "0000"),
+    ("testuser", "1234")
+]
+
+for username, password in users:
+    result = user_login(username, password)
+
+    if result == "success":
+        print("Testing user:", username, "Passed")
+    else:
+        print("Testing user:", username, "Failed")
